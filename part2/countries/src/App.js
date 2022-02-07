efe30d5fd99b40ac28ac3a9380c9efede6e1531f
Tom Radford <tom@theradford.com>
2022-02-07 15:12:45 +0200

    excercise 2.11-2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,27 +1,86 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-
-const Filter = (props) => (
-  <p>find countries <input value={props.countriesFilter} onChange={props.handleFilter} /> </p>
+const Filter = ({ handleFilter }) => (
+  <p>find countries <input onChange={handleFilter} /> </p>
 )
 
-const Country = ({ countriesToShow }) => {
+const Weather = ({ countryDetails }) => {
+  const [weatherData, setWeatherData] = useState({})
+  const api_key = process.env.REACT_APP_WEATHER_API_KEY
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${countryDetails.capitalInfo.latlng[0]}&lon=${countryDetails.capitalInfo.latlng[1]}&appid=${api_key}&units=metric`)
+      .then(response => {
+        setWeatherData(response.data)
+      })
+  }, [])
 
-  console.log(countriesToShow)
+  const compass = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
 
-  const countryList = () => (
-    <ul>
-      {countriesToShow.map(country => {
-        <li key={country.name.common}>country.name.common</li>
-      })}
+  const degToCompass = (degrees) => {
+    const val = Math.floor((degrees / 22.5) + 0.5)
+    return compass[(val % 16)]
+
+  }
+
+  if (Object.keys(weatherData).length !== 0) {
+
+    const windDirection = degToCompass(weatherData.wind.deg)
+    return (
+      <div>
+        <p><b>temperature: </b>{weatherData.main.temp} Celcius</p>
+        <img style={{ background: '#3d4c92' }} alt="" src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}></img>
+        <p><b>wind: </b>{Math.round(weatherData.wind.speed * 2.23694)} mph direction {windDirection}</p>
+      </div>
+    )
+  } else return null
+}
+
+const CountryDisplay = ({ countryDetails }) => {
+  const countryLanguages = Object.values(countryDetails.languages)
+  return (
+    <div>
+      <h1>{countryDetails.name.common}</h1>
+      <p>capital {countryDetails.capital[0]}</p>
+      <p>population {countryDetails.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {countryLanguages.map(language =>
+          <li key={language}>{language}</li>)}
       </ul>
-  )
+      <img alt=" " src={countryDetails.flags.png} />
+      <h2>Weather in {countryDetails.capital[0]}</h2>
 
-  if (countriesToShow.length >= 2 && countriesToShow.length <= 10) 
-    {countryList()} 
-  if (countriesToShow.length > 10) (<>Too many matches, specify another filter</>)
-  else return null
+      <Weather countryDetails={countryDetails} />
+
+    </div>
+  )
+}
+
+const CountryList = ({ countriesToShow, countriesFilter, setCountriesFilter }) => {
+  if (countriesFilter !== '') {
+    if (countriesToShow.length === 1) {
+      const countryDetails = countriesToShow[0]
+      return (
+        <CountryDisplay countryDetails={countryDetails} />
+      )
+    }
+    else if (countriesToShow.length <= 10) {
+      return (
+        <div>
+          <ul>
+            {countriesToShow.map(country =>
+              <li key={country.name.common}>{country.name.common} <button onClick={() => setCountriesFilter(country.name.common)}>show</button></li>
+            )}
+          </ul>
+        </div>)
+    } else if (countriesToShow.length > 10) {
+      return (
+        <p>Too many matches, specify another filter</p>
+      )
+    }
+  } else return null
 }
 
 
@@ -30,7 +89,6 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
   const [countriesFilter, setCountriesFilter] = useState('')
-  console.log(countriesFilter)
 
   const handleFilter = (event) => {
     setCountriesFilter(event.target.value)
@@ -48,13 +106,13 @@ const App = () => {
   const countriesToShow = countries.filter(country =>
     country.name.common
       .toLowerCase()
-      .includes(countriesFilter)
+      .includes(countriesFilter.toLowerCase())
   )
 
   return (
     <div>
       <Filter countriesFilter={countriesFilter} handleFilter={handleFilter} />
-      <Country countriesToShow={countriesToShow} />
+      <CountryList countriesToShow={countriesToShow} countriesFilter={countriesFilter} setCountriesFilter={setCountriesFilter} />
     </div>
   )
 }
